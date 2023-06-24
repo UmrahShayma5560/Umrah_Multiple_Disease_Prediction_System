dd50a6ebc50417fb2d632f3764034b234e1af2c7
shivam kumar <shivamkrbu005@gmail.com>
2023-06-24 17:04:26 +0530

    Shivam

---
+++ Flask(Backend Part)/Diabetes/diabetes.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+model = pickle.load(open('diabetes_model.pkl', 'rb'))
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def man():
+    return render_template('diabetes.html')
+
+
+@app.route('/predict', methods=['POST'])
+def home():
+    data1 = request.form['a']
+    data2 = request.form['b']
+    data3 = request.form['c']
+    data4 = request.form['d']
+    data5 = request.form['e']
+    data6 = request.form['f']
+    data7 = request.form['g']
+    data8 = request.form['h']
+    arr = np.array([[data1, data2, data3, data4, data5, data6, data7, data8]], dtype=float)
+    pred = model.predict(arr)
+    return render_template('diabetes_after.html', data=pred)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/index.js
@@ -0,0 +1,40 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slideImg_div");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
+//best product slider javascript**********************************
+
+
+let cardbtnright = document.getElementById("bpnext")
+cardbtnright.addEventListener("click",function(){
+  // console.log("click right")
+  transform_info.style.transform = "translateX(-30%)"
+})
